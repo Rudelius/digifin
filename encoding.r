e37da1a932a3ba2b9c0500b6a65f835b25f818d3
Johan Rudelius <johrud@kth.se>
2019-02-22 23:16:17 +0100

    added neural network model

--- encoding.r
+++ encoding.r
@@ -1,14 +1,19 @@
+# Dependencies: install.packages(dplyr", "utf8", "tree", "randomForest", "gbm", "keras")
 library(dplyr)
+library(utf8)
 library(tree)
 library(randomForest)
-library(utf8)
-set.seed(1)
+library(gbm)
+library(keras)
+library(reticulate)
+use_python("/usr/bin/python")
+#set.seed(1)
 
 # Plot two plots at a time.
 par(mfrow= c(1,2))
 
 # Clean environment.
-rm(list = ls())
+#rm(list = ls())
 
 # Import unmodified loandata. 
 loandata <- read.csv(file="/Users/johan/Dropbox/Handels/digifin/data/loandata.csv", header = TRUE, sep=",")
@@ -114,10 +119,6 @@ plot(tree.loandata.pruned)
 text(tree.loandata.pruned, pretty = 0)
 title(main = "Pruned")
 
-
-
-####################################### Tree evaluating part ######################################
-
 # Calculate percentage of correct predictions for unpruned tree.
 pred.tree.loandata <- predict(tree.loandata, loandata.test, type = "vector")
 correctPredictions = 0
@@ -148,7 +149,7 @@ meanError.tree.loandata.pruned <- mean(abs(pred.tree.loandata.pruned-loandata.te
 stats <- matrix(c(meanError.tree.loandata, correctPredictions.tree.loandata,
                   meanError.tree.loandata.pruned, correctPredictions.tree.loandata.pruned)
                 ,ncol = 2)
-colnames(stats) <- c('unpruned', 'pruned')
+colnames(stats) <- c('unpruned tree', 'pruned tree')
 rownames(stats) <- c('meanError', 'correctPredictions')
 as.table(stats)
 
@@ -157,21 +158,18 @@ as.table(stats)
 #################################### Bagging part ######################################
 
 # Necessary fixing of NA values for random forest to work, drop lastucrequest, and add NA factor level.
-# loandata.test <- loandata.test[!is.na(loandata.test$lastucrequest),] old
 loandata.test$lastucrequest <- NULL
 loandata.train$lastucrequest <- NULL
 loandata.test <- na.tree.replace(loandata.test)
 
 # Make a bagging model.
-bagg.loandata = randomForest(x = loandata.train[,1:24], y = loandata.train$newpayingremark, 
-                            subset = trainingRows, mtry = length(loandata.train)-1,
-                            importance = TRUE, ntree = 100)
-par(mfrow= c(1,1))
-plot(bagg.loandata, main = "OOB error to number of trees")
-par(mfrow= c(1,2))
+bagg.loandata = randomForest(x = loandata.train[,names(loandata.train) != "newpayingremark"], y = loandata.train$newpayingremark,
+                             mtry = length(loandata.train)-1, importance = TRUE, ntree = 100)
 
-# Evaluate Bagging model.
-pred.bagg.loandata <- predict(bagg.loandata, newdata = loandata.test)
+# Plot the OOB error to number of trees.
+par(mfrow= c(1,1))
+plot(bagg.loandata, main = "Bagging OOB error to number of trees")
+par(mfrow= c(1,2))
 
 # Calculate percentage of correct predictions for bagging model.
 pred.bagg.loandata <- predict(bagg.loandata, newdata = loandata.test)
@@ -189,7 +187,182 @@ meanError.bagg.loandata <- mean(abs(pred.bagg.loandata-loandata.test$newpayingre
 # Add the new statistics to the stats table.
 tempMatrix <- matrix(c(meanError.bagg.loandata, correctPredictions.bagg.loandata))
 stats <- cbind(stats, tempMatrix)
-colnames(stats) <- c('unpruned', 'pruned', 'bagging')
+colnames(stats) <- c('unpruned tree', 'pruned tree', 'bagging')
 as.table(stats)
 
+# Plot relative importance of input features.
+varImpPlot (bagg.loandata )
+
+
+
+#################################### Random forest part ######################################
+
+# Make a random forest model.
+randomForest.loandata <- randomForest(x = loandata.train[,names(loandata.train) != "newpayingremark"],
+                                      y = loandata.train$newpayingremark, importance = TRUE, ntree = 100)
+
+# Plot the OOB error to number of trees.
+par(mfrow= c(1,1))
+plot(randomForest.loandata, main = "Random forest OOB error to number of trees")
+par(mfrow= c(1,2))
+
+# Calculate percentage of correct predictions for random forest model.
+pred.randomForest.loandata <- predict(randomForest.loandata, newdata = loandata.test)
+correctPredictions = 0
+for (i in 1:length(pred.randomForest.loandata)){
+  if(abs(pred.randomForest.loandata[i]-loandata.test$newpayingremark[i]) < 0.5){
+    correctPredictions = correctPredictions + 1
+  } 
+}
+correctPredictions.randomForest.loandata <- correctPredictions / length(loandata.test[,1])
+
+# Calculate the mean error of predictions for bagging model.
+meanError.randomForest.loandata <- mean(abs(pred.randomForest.loandata-loandata.test$newpayingremark))
+
+# Add the new statistics to the stats table.
+tempMatrix <- matrix(c(meanError.randomForest.loandata, correctPredictions.randomForest.loandata) , ncol = 1)
+stats <- cbind(stats, tempMatrix)
+colnames(stats) <- c('unpruned tree', 'pruned tree', 'bagging', 'randomForest')
+as.table(stats)
+
+# Plot relative importance of input features.
+varImpPlot (randomForest.loandata )
+
+
+#################################### Boosting part ######################################
+
+boost.loandata = gbm(newpayingremark~ ., data = loandata.train, cv.folds = 10,
+                 n.trees = 100, interaction.depth = 3, distribution = "gaussian")
+ 
+summary(boost.loandata)
+
+par(mfrow=c(1,2))
+plot(boost.loandata ,i="noinquiries")
+plot(boost.loandata ,i="ucriskperson")
+plot(boost.loandata ,i="creationdateuserloan")
+
+# Calculate percentage of correct predictions for boosting model.
+pred.boost.loandata <- predict(boost.loandata, newdata = loandata.test, n.trees = 85)
+correctPredictions = 0
+for (i in 1:length(pred.boost.loandata)){
+  if(abs(pred.boost.loandata[i]-loandata.test$newpayingremark[i]) < 0.5){
+    correctPredictions = correctPredictions + 1
+  } 
+}
+correctPredictions.boost.loandata <- correctPredictions / length(loandata.test[,1])
+
+# Calculate the mean error of predictions for boosting model.
+meanError.boost.loandata <- mean(abs(pred.boost.loandata-loandata.test$newpayingremark))
+
+# Add the new statistics to the stats table.
+tempMatrix <- matrix(c(meanError.boost.loandata, correctPredictions.boost.loandata), ncol = 1)
+stats <- cbind(stats, tempMatrix)
+colnames(stats) <- c('unpruned tree', 'pruned tree', 'bagging', 'randomForest', 'boosting')
+
+# Print info on boost model.
+boost.loandata
+
+# Print stats table.
+as.table(stats)
+
+######################################## Data splitting and final cleaning part #######################################
+
+loandata$lastucrequest <- NULL
+loandata <- na.tree.replace(loandata)
+
+# Replace region data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$lan)), loandata)
+loandata$lan <- NULL
+
+# Replace category data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$category)), loandata)
+loandata$category <- NULL
+
+# Replace gender data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$gender)), loandata)
+loandata$gender <- NULL
+
+# Replace gender data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$maritalstatus)), loandata)
+loandata$maritalstatus <- NULL
+
+# Replace jobstatus data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$jobstatus)), loandata)
+loandata$jobstatus <- NULL
+
+# Replace jobtype data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$jobtype)), loandata)
+loandata$jobtype <- NULL
+
+# Replace livingstatus data with one hot encoding.
+loandata <- cbind(to_categorical(as.numeric(loandata$livingstatus)), loandata)
+loandata$livingstatus <- NULL
+
+# Standardise data.
+loandata.scaled <- scale(loandata)
+
+# Split loandata into training and test sets.
+x.train <- as.matrix( loandata[trainingRows, 1:114] )
+y.train <- as.matrix( loandata$newpayingremark[trainingRows] )
+
+x.test <- as.matrix( loandata[-trainingRows, 1:114] )
+y.test <- as.matrix( loandata$newpayingremark[-trainingRows] )
+
+
+
+######################################## Neural network part #######################################
+
+model <- keras_model_sequential() 
+model %>% 
+  layer_dense(units = 3000, activation = 'sigmoid', kernel_initializer = 'normal') %>% 
+  layer_dropout(rate = 0.5) %>% 
+  layer_dense(units = 1500, activation = 'relu', kernel_initializer = 'normal') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 750, activation = 'relu', kernel_initializer = 'normal') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 375, activation = 'relu', kernel_initializer = 'normal') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 1, activation = 'linear', kernel_initializer = 'normal')
+
+model %>% compile(
+  loss = 'mean_squared_error',
+  optimizer = optimizer_rmsprop()
+)
+
+history <- model %>% fit(
+  x.train, y.train, 
+  epochs = 25,
+  batch_size = 128, 
+  validation_split = 0.1
+)
+plot(history)
+model %>% evaluate(x.test, y.test)
+
+pred.nn.loandata <- model %>% predict(x.test)
+
+# Plot distributions from different models.
+plot(pred.bagg.loandata)
+plot(pred.randomForest.loandata)
+plot(pred.boost.loandata)
+plot(pred.nn.loandata)
+
+# Calculate percentage of correct predictions for boosting model.
+correctPredictions = 0
+for (i in 1:length(pred.nn.loandata)){
+  if(abs(pred.nn.loandata[i]- y.test[i]) < 0.5){
+    correctPredictions = correctPredictions + 1
+  }
+}
+correctPredictions.nn.loandata <- correctPredictions / length(y.test)
+
+meanError.nn.loandata <- mean(abs(pred.nn.loandata-y.test))
+
+# Add the new statistics to the stats table.
+tempMatrix <- matrix(c(meanError.nn.loandata, correctPredictions.nn.loandata), ncol = 1)
+stats <- stats[,-6]
+stats <- cbind(stats, tempMatrix)
+colnames(stats) <- c('unpruned tree', 'pruned tree', 'bagging', 'randomForest', 'boosting', 'neural net')
+
+# Print stats table.
+as.table(stats)
 
